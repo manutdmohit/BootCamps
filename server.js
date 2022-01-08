599c2a9cd88c5c2b54b9f0ceaa0e1494bd4571d5
manutdmohit <saudmohit@gmail.com>
2022-01-08 19:36:23 +0545

    creating routes responses in express

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const res = require('express/lib/response');
 
 // load config vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `show bootcamp ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, id: 1, msg: 'create new bootcamp' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `update bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
 
 const PORT = process.env.PORT || 8000;
 
