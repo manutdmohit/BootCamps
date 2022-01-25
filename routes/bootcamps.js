d5528326cc43027d2e14cb6bb752dbe0edcd30d8
manutdmohit <saudmohit@gmail.com>
2022-01-25 11:16:22 +0545

    courses routes controllers

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,7 +9,13 @@ const {
   getBootcampsByRadius,
 } = require('../controllers/bootcamps');
 
+// Include other resource routers
+const courseRouter = require('./courses');
+
 const router = express.Router();
+
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsByRadius);
 
