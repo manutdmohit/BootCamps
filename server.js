164b1871d36027a57a335c93afd4d3eec1611498
manutdmohit <saudmohit@gmail.com>
2022-02-01 14:23:14 +0545

    User Model

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ connectDB();
 // Route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 const app = express();
 
@@ -32,11 +33,12 @@ if (process.env.NODE_ENV == 'development') {
 app.use(fileupload());
 
 // Set static folder
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 app.use(errorHandler);
 
