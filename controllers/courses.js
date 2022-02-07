08ae24272da11556c48fefd0fe0b020f329cd357
manutdmohit <saudmohit@gmail.com>
2022-02-07 11:42:55 +0545

    edited comment from course to courses

--- controllers/courses.js
+++ controllers/courses.js
@@ -4,7 +4,7 @@ const asyncHandler = require('../middleware/async');
 const Course = require('../models/Course');
 const Bootcamp = require('../models/Bootcamp');
 
-// @desc Get course
+// @desc Get courses
 // @route GET /api/v1/courses
 // @route GET /api/v1/bootcamps/:bootcampId/courses
 // @access Public
