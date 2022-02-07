4524554f6293efd3a98e413f58020fb3f2bb2f2a
manutdmohit <saudmohit@gmail.com>
2022-02-07 14:16:35 +0545

    Get single review update seeder

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -4,7 +4,7 @@ const asyncHandler = require('../middleware/async');
 const Review = require('../models/Review');
 const Bootcamp = require('../models/Bootcamp');
 
-// @desc Get course
+// @desc Get reviews
 // @route GET /api/v1/reviews
 // @route GET /api/v1/bootcamps/:bootcampId/reviews
 // @access Public
@@ -20,4 +20,25 @@ exports.getReviews = asyncHandler(async (req, res, next) => {
   } else {
     res.status(200).json(res.advancedResults);
   }
+});
+
+// @desc Get single review
+// @route GET /api/v1/reviews/:id
+// @access Public
+exports.getReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: 'name description',
+  });
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review found with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: review,
+  });
 });
